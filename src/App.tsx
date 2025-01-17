38b4566b463dab05c689445d991bfb04aa1dbb40
Dyr0tH <shahid.ali.gtx@gmail.com>
2025-01-17 16:30:59 +0530

    updated the grid columns on home page

--- src/App.tsx
+++ src/App.tsx
@@ -101,7 +101,7 @@ function HomePage() {
             }
           `}
         </style>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 relative">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 relative">
           {features.map((feature, index) => (
             <div
               key={index}
